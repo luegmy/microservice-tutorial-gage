92e6744b012a520106561ce90ae814e75527d9b2
Jhonny Neyra Lopez <jneyralo@everis.com>
2023-04-15 22:32:39 -0500

    configuracion de eureka

--- user-service/src/main/java/com/tutorial/userservice/feignclients/BikeFeignClients.java
+++ user-service/src/main/java/com/tutorial/userservice/feignclients/BikeFeignClients.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "bike-service", url = "http://localhost:8001")
+@FeignClient(name = "bike-service")
 @RequestMapping("/bikes")
 public interface BikeFeignClients {
 
