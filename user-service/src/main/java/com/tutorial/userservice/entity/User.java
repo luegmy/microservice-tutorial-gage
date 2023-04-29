55578890ad1253fb1b344874ffa3c97b9ee92f7a
Jhonny Neyra Lopez <jneyralo@everis.com>
2023-04-29 10:25:36 -0500

    modify colecciones de mongo para user

--- user-service/src/main/java/com/tutorial/userservice/entity/User.java
+++ user-service/src/main/java/com/tutorial/userservice/entity/User.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("cars")
+@Document("users")
 @Getter
 @Setter
 public class User {
